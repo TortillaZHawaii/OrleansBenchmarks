28ddbdca8fade15da94b2d32a7e144de3cee5d82
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-04-17 13:44:45 +0200

    Add logging to bash script

--- OrleansBenchmarks.ClientServer/runclientserver.sh
+++ OrleansBenchmarks.ClientServer/runclientserver.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+echo "Starting server"
+
 # start the server in the background and get its pid
 dotnet OrleansBenchmarks.ClientServer.Server.dll &
 server_pid=$!
@@ -5,8 +8,14 @@ server_pid=$!
 # wait for the server to start
 sleep 10
 
+echo "Starting client"
+
 # start the client
 dotnet OrleansBenchmarks.ClientServer.Client.dll
 
+echo "Client finished"
+
 # kill the server
 kill $server_pid
+
+echo "Server finished"
